b219a3d337425df4c07c46b81cac9d6ec69b9f95
Captain0X <35555262+Captain0X@users.noreply.github.com>
2022-11-21 22:40:00 +0800

    Update multibyte_trans_tool.py
    update trans style

--- multibyte_trans_tool.py
+++ multibyte_trans_tool.py
@@ -3,23 +3,29 @@
  @author : Captain0X
  @time : 2022/11/21 16:54
  '''
-from urllib.parse import quote
 from string import punctuation
+
 def trans_multi_encoding(char_str):
     multi_char=""
     for char in char_str:
         if char in punctuation:
-            multi_char+=bytes('\\u56' + quote(char).replace("%", ""), 'utf-8').decode('raw_unicode_escape')
+            multi_char+=bytes('\\u56' +'%02x' % ord(char),encoding="utf-8").decode('raw_unicode_escape')
         else:
             multi_char+=char
-    print(multi_char)
+    print('-------------------------------\noutput:\n',multi_char)
 banner="""
-trans_multi_encoding("<% =7 * 7 %>")
+input:
+<% =7 * 7 %>
+
 output:
 嘼嘥 嘽7 嘪 7 嘥嘾
 you can copy this result to burp ,burp will trans to code auto~
         tools_code_by_Captain0X
 
 """
-code=input("please input your code:")
-trans_multi_encoding(code)
+print(banner)
+while True:
+    code=input("please input your code(input 0 exit):")
+    if code=='0':
+        exit()
+    trans_multi_encoding(code)
